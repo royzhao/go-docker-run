4e528532c6c8b26a0e560ba001722a2602609e50
royzhao <524510356@qq.com>
2015-04-03 11:20:03 +0800

    add time limit

--- util.go
+++ util.go
@@ -1,48 +1,69 @@
 package main
 
 import (
-	// "fmt"
-	// "io/ioutil"
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-	// "time"
+	"strings"
+	"time"
 )
+
+func printCommand(cmd *exec.Cmd) {
+	fmt.Printf("==> Executing: %s\n", strings.Join(cmd.Args, " "))
+}
+
+func printError(err error) {
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
+	}
+}
+
+func printOutput(outs []byte) {
+	if len(outs) > 0 {
+		fmt.Printf("==> Output: %s\n", string(outs))
+	}
+}
 
 func run_command(name string, args []string, input string) (string, error) {
 
-	var output bytes.Buffer
+	log.Println("run command" + name)
 	c := exec.Command(name, args...)
-	c.Stdout = &output
-	stdin, err := c.StdinPipe()
+	e, err := c.StderrPipe()
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return "", err
+		return "error start cmd: " + name, err
 	}
-	i, err := c.StderrPipe()
+	o, err := c.StdoutPipe()
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return "", err
+		return "error start cmd: " + name, err
+	}
+	err = c.Start()
+	if err != nil {
+		return "error start cmd: " + name, err
+	}
+	//start a timer,limit 5s
+	timer := time.NewTicker(time.Second * 5)
+	go func() {
+		<-timer.C
+		_, err := c.Stderr.Write([]byte("run too long"))
+		if err != nil {
+			log.Println(err)
+		}
+		c.Process.Signal(os.Kill)
+		log.Println("cancel run command:" + name)
+	}()
+	ebyte, _ := ioutil.ReadAll(e)
+	obyte, _ := ioutil.ReadAll(o)
+	err = c.Wait()
+	//cancel timer
+	timer.Stop()
+	log.Println("run command over")
+	if err != nil {
+		return string(ebyte), err
 	}
 
-	if err = c.Start(); err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return "", err
-	}
-	if input != "" {
-		stdin.Write([]byte(input))
-		stdin.Close()
-	}
-	b, _ := ioutil.ReadAll(i)
-	if err := c.Wait(); err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return "", err
-	}
-	fmt.Println(string(b))
-	return string(b), nil
+	return string(obyte), nil
 
 }
 func put_code_into_file(filename string, code string) error {
@@ -74,14 +95,16 @@ func find_cmd_root(cmd string, wd string) (string, error) {
 	}
 	return f, nil
 }
+func CleanDir(path string) error {
+	err := os.RemoveAll(path)
+	return err
+}
 func run(id *Run) {
 	log.Println("running" + id.Id)
 	//mkdir workspace
 	id.WorkDir = "/home/zpl/docker-run-test/" + id.Id + "/"
 	err := add_work_path(id.WorkDir)
 	if err != nil {
-		// return http.StatusServiceUnavailable, Must(enc.Encode(
-		// 	NewError(ErrRunCmd, fmt.Sprintf("server error in create workspace"))))
 		log.Println("server error in create workspace")
 		log.Println(err)
 		return
@@ -89,8 +112,6 @@ func run(id *Run) {
 	log.Println("create dir " + id.WorkDir + " successful")
 	err = os.Chdir(id.WorkDir)
 	if err != nil {
-		// return http.StatusServiceUnavailable, Must(enc.Encode(
-		// 	NewError(ErrRunCmd, fmt.Sprintf("server error in create workspace"))))
 		log.Println("server error in change  workspace")
 		log.Println(err)
 		return
@@ -98,8 +119,6 @@ func run(id *Run) {
 	log.Println("change dir " + id.WorkDir + " successful")
 	err = put_code_into_file(id.WorkDir+id.Code.Filename, id.Code.Content)
 	if err != nil {
-		// return http.StatusServiceUnavailable, Must(enc.Encode(
-		// 	NewError(ErrRunCmd, fmt.Sprintf("server error in create code file"))))
 		log.Println("server error in create code file")
 		log.Println(err)
 		return
@@ -109,78 +128,23 @@ func run(id *Run) {
 	for _, v := range id.Cmds {
 		f, err := find_cmd_root(v.Cmd, id.WorkDir)
 		if err != nil {
-			// return http.StatusNotAcceptable, Must(enc.Encode(
-			// 	NewError(ErrRunCmd, fmt.Sprintf("not found command %s", v.Cmd))))
 			log.Println("not found command " + v.Cmd)
 			return
 		}
 		res, err := run_command(f, v.Args, "")
 		if err != nil {
-			log.Println("error in run cmd" + v.Cmd)
+			log.Println("==================")
+			log.Println("run cmd " + v.Cmd + " error")
+			log.Println(res)
 			log.Println(err)
+			log.Println("******************")
 			return
 		}
-
 		log.Println("cmd " + v.Cmd + " run result :" + res)
 	}
-	// cmd := exec.Command("/bin/sh", "-c", "ping 127.0.0.1")
-	// _, err := cmd.Output()
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-
-	// if err := cmd.Start(); err != nil {
-	// 	panic(err.Error())
-	// }
-
-	// if err := cmd.Wait(); err != nil {
-	// 	panic(err.Error())
-	// }
+	//clean
+	err = CleanDir(id.WorkDir)
+	if err != nil {
+		log.Println("Clean workspace is failed!")
+	}
 }
-
-// func main() {
-// 	go run()
-// 	time.Sleep(1e9)
-
-// 	cmd := exec.Command("/bin/sh", "-c", `ps -ef | grep -v "grep" | grep "ping"`)
-// 	stdout, err := cmd.StdoutPipe()
-// 	if err != nil {
-// 		fmt.Println("StdoutPipe: " + err.Error())
-// 		return
-// 	}
-
-// 	stderr, err := cmd.StderrPipe()
-// 	if err != nil {
-// 		fmt.Println("StderrPipe: ", err.Error())
-// 		return
-// 	}
-
-// 	if err := cmd.Start(); err != nil {
-// 		fmt.Println("Start: ", err.Error())
-// 		return
-// 	}
-
-// 	bytesErr, err := ioutil.ReadAll(stderr)
-// 	if err != nil {
-// 		fmt.Println("ReadAll stderr: ", err.Error())
-// 		return
-// 	}
-
-// 	if len(bytesErr) != 0 {
-// 		fmt.Printf("stderr is not nil: %s", bytesErr)
-// 		return
-// 	}
-
-// 	bytes, err := ioutil.ReadAll(stdout)
-// 	if err != nil {
-// 		fmt.Println("ReadAll stdout: ", err.Error())
-// 		return
-// 	}
-
-// 	if err := cmd.Wait(); err != nil {
-// 		fmt.Println("Wait: ", err.Error())
-// 		return
-// 	}
-
-// 	fmt.Printf("stdout: %s", bytes)
-// }
